7f1d8907fb1e1d5dd15b125fcb269b1321fb4b95
EC2 Default User <ec2-user@ip-172-31-18-40.us-east-2.compute.internal>
2019-11-08 18:05:57 +0000

    add answers

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,11 +1,34 @@
 // Question 1
-function stripPunctuation() {
-
+function stripPunctuation(string) {
+    let newString = ""; 
+    let arr = string.split("");
+    let alphabet = /^[a-z0-9]+$/i;     //used a regex 
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i].match(alphabet)){
+            newString += arr[i];
+        }
+        else{
+            delete arr[i];
+        }
+    }return newString;
 }
 
 // Question 2
-function rotateArray() {
-
+function rotateArray(arr1) {
+    const arr2 = arr1.slice();
+    let removeFirst;
+    let newArr;
+    
+    if(!Array.isArray(arr1)){
+        return undefined;
+    }
+    else if (arr1.length < 1) {
+        return arr1;
+    }
+    else{
+        removeFirst = arr2.shift();
+        newArr = arr2.push(removeFirst);
+    }return newArr;
 }
 
 // Question 3
