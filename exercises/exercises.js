2b628e56f5664f46e86434504f98f9a6674c3c23
Seal125 <stephaniesmith12514@gmail.com>
2019-11-08 17:59:31 +0000

    Unit 2 Assessment

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,16 +1,53 @@
 // Question 1
-function stripPunctuation() {
+function stripPunctuation(str) {
+  const newStr = str.split('');
+  let result = '';
 
+  for (let i = 0; i < newStr.length; i += 1) {
+    console.log(newStr);
+    if ((newStr[i] >= 'a' && newStr[i] <= 'z') || (newStr[i] >= 'A' && newStr[i] <= 'Z')) {
+      result += newStr[i];
+    } else if (Number(newStr[i])) {
+      result += newStr[i];
+    }
+  }
+  return result;
 }
 
 // Question 2
-function rotateArray() {
+function rotateArray(array) {
+  if (array === -1) {
+    return [];
+  }
+  if (!Array.isArray(array)) {
+    return undefined;
+  }
 
+  const arrayCopy = array.slice();
+  arrayCopy.push(arrayCopy.shift());
+  return arrayCopy;
 }
 
 // Question 3
-function letterCaseCounts() {
+function letterCaseCounts(str) {
+  const obj = {
+    lowercase: 0,
+    uppercase: 0,
+    neither: 0,
+  };
 
+  const strArr = str.split('');
+
+  for (let i = 0; i < strArr.length; i += 1) {
+    if (strArr[i] >= 'A' && strArr[i] <= 'Z') {
+      obj.uppercase += 1;
+    } else if (strArr[i] >= 'a' && strArr[i] <= 'z') {
+      obj.lowercase += 1;
+    } else {
+      obj.neither += 1;
+    }
+  }
+  return obj;
 }
 
 // Don't write below this line...
