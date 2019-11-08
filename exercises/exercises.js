ed4259f667fe9c2d3e90a46bdc37ebf2d135768a
EC2 Default User <ec2-user@ip-172-31-18-93.us-east-2.compute.internal>
2019-11-08 18:03:40 +0000

    test

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,11 +1,28 @@
 // Question 1
-function stripPunctuation() {
-
+function stripPunctuation(sentence) {
+ let lowercaseletters = 'abcdefghijklmnopqrstuvwxyz';
+  let uppercaseletters = lowercaseletters.toUpperCase();
+  let numbers = '1234567890';
+  let together = lowercaseletters.concat(uppercaseletters,numbers).split('')
+  let newstring = sentence.split('');
+  let final = [];
+  for(let i = 0; i < newstring.length; i++){
+    if(together.includes(newstring[i])){
+       final = final + newstring[i];
+    }else{
+      delete newstring[i]
+    }
+}
+  return final;
 }
 
 // Question 2
-function rotateArray() {
-
+function rotateArray(original) {
+let copy = [...original];
+let x = original[0];
+copy.shift();
+copy.push(x)
+console.log(copy)
 }
 
 // Question 3
