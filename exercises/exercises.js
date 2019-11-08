098dc49bfd3585944ab725c25de77e66f5e64e17
EC2 Default User <ec2-user@ip-172-31-19-228.us-east-2.compute.internal>
2019-11-08 18:00:53 +0000

    submission

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,17 +1,44 @@
 // Question 1
 function stripPunctuation() {
-
+    str = str.split(' ');
+    for (let i =0; i < str.length; i += 1){
+    }
 }
 
 // Question 2
-function rotateArray() {
-
+function rotateArray(array){
+    let newArr = [];
+    for(let i = 0; i < array.length; i += 1){
+        let del = array.shift();
+        newArr = array.push(del);
+    }
+    return newArr;
 }
+// Code above do not work.
+let myArr = [1,2,3,4];
+rotateArray(myArr);   
+
 
 // Question 3
-function letterCaseCounts() {
+function letterCaseCounts(name){    
+    const stats = {};
 
+    stats[`lowercase`] = 0;
+    stats[`uppercase`] = 0;
+    stats[`neither`]= 0;
+
+    for(let i =0; i< name.length; i++){
+        if(name[i] >= 'A' && name[i] <= 'Z'){
+            stats[`uppercase`] ++;
+        } else if(name[i] >= 'a' && name[i] <= 'z'){
+            stats[`lowercase`] ++;
+        } else if(name[i] >= 0 && name[i] <= 9){
+            stats[`neither`] ++;
+        }
+    }
+    return stats;
 }
+//Kind of work 
 
 // Don't write below this line...
 module.exports = { stripPunctuation, rotateArray, letterCaseCounts };
