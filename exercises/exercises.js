385cd37d238a53d1e536e547275c9fe6fcfb5977
Mark Griffith <markgriffith009@Gmail.com>
2019-11-08 17:02:24 +0000

    Eslintified.

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,56 +1,54 @@
 // Question 1
 function stripPunctuation(str) {
-let newString = '';
- for (let letter of str) {
-        if (letter.charCodeAt() > 64 && letter.charCodeAt() < 91) {
-            newString += letter;
-        }
-        if(letter.charCodeAt() > 96 && letter.charCodeAt() < 123){
-            newString += letter;
-        }
-        if(letter.charCodeAt() > 47 && letter.charCodeAt() < 58){
-            newString += letter;
-        }
+  let newString = '';
+  for (const letter of str) {
+    if (letter.charCodeAt() > 64 && letter.charCodeAt() < 91) {
+      newString += letter;
     }
-    return newString;
+    if (letter.charCodeAt() > 96 && letter.charCodeAt() < 123) {
+      newString += letter;
+    }
+    if (letter.charCodeAt() > 47 && letter.charCodeAt() < 58) {
+      newString += letter;
+    }
+  }
+  return newString;
 }
 
 // Question 2
 function rotateArray(arr) {
-    if(Array.isArray(arr) && arr.length === 0){
-        return [];
-    }
-    else if(!Array.isArray(arr)){
-        return undefined;
-    }
-    let newArr = arr.slice();
-    let item = newArr.shift();
-    
-    newArr.push(item);
-    return newArr;
-  
+  if (Array.isArray(arr) && arr.length === 0) {
+    return [];
+  }
+  if (!Array.isArray(arr)) {
+    return undefined;
+  }
+  const newArr = arr.slice();
+  const item = newArr.shift();
+
+  newArr.push(item);
+  return newArr;
 }
 // Question 3
 function letterCaseCounts(str) {
-   let count = { 
-       lowercase: 0, 
-       uppercase: 0, 
-       neither: 0 };
-     for(let letter of str){
-         if(letter.charCodeAt() > 64 && letter.charCodeAt() < 91){
-             count.uppercase += 1;
-             continue;
-         }
-         if(letter.charCodeAt() > 96 && letter.charCodeAt() < 123){
-             count.lowercase += 1;
-             continue;
-         }
-         else{
-             count.neither += 1;
-         }
-     }
-     return count;
-
+  const count = {
+    lowercase: 0,
+    uppercase: 0,
+    neither: 0,
+  };
+  for (const letter of str) {
+    if (letter.charCodeAt() > 64 && letter.charCodeAt() < 91) {
+      count.uppercase += 1;
+      continue;
+    }
+    if (letter.charCodeAt() > 96 && letter.charCodeAt() < 123) {
+      count.lowercase += 1;
+      continue;
+    } else {
+      count.neither += 1;
+    }
+  }
+  return count;
 }
 // Don't write below this line...
 module.exports = { stripPunctuation, rotateArray, letterCaseCounts };
