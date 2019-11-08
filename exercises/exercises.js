5613322e35238d3f7f7ba0b3a89c592bd5343cb3
EC2 Default User <ec2-user@ip-172-31-32-161.us-east-2.compute.internal>
2019-11-08 18:02:21 +0000

    closing

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,65 +1,61 @@
 // Question 1
 function stripPunctuation(string) {
-  let newString = string.split("")
-  let finalString = "";
+  const newString = string.split('');
+  let finalString = '';
 
   for (i = 0; i < newString.length; i++) {
-    
-    if(newString[i].match(/^[a-z0-9]+$/i) !== null) {  // Checks if Any letters A-Z or Numbers 0-9 are present within string
-      finalString = finalString + newString[i]
-      
-   }
- }
- return finalString
+    if (newString[i].match(/^[a-z0-9]+$/i) !== null) { // Checks if Any letters A-Z or Numbers 0-9 are present within string
+      finalString += newString[i];
+    }
+  }
+  return finalString;
 }
 
 // Question 2
 function rotateArray(array) {
-
-  let newArray = []; //empty
-  newArray = array; //my New Array
+  let newArray = []; // empty
+  newArray = array; // my New Array
   let firstElement;
-  firstElement = newArray[0]
-  let finalArray = []; // The output, empty array for now
+  firstElement = newArray[0];
+  const finalArray = []; // The output, empty array for now
 
-// Conditionals
-if (Array.isArray(newArray) == false || undefined) return undefined; //end it here
-if (newArray == []) return [];//dont even run it
+  // Conditionals
+  if (Array.isArray(newArray) == false || undefined) return undefined; // end it here
+  if (newArray == []) return [];// dont even run it
 
 
-for(i = 1; i < newArray.length; i++) {
-  finalArray[i - 1] = newArray[i];
-}
+  for (i = 1; i < newArray.length; i++) {
+    finalArray[i - 1] = newArray[i];
+  }
   finalArray[newArray.length - 1] = firstElement;
-  return finalArray
+  return finalArray;
 }
 
 
 // Question 3
 function letterCaseCounts(string) {
-    
-    let lowCounter = 0;
-    let upperCounter = 0;
-    let neighterCounter = 0;
-    let neighterRegex = /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]+$/g
+  let lowCounter = 0;
+  let upperCounter = 0;
+  let neighterCounter = 0;
+  const neighterRegex = /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]+$/g;
 
-    upperCounter = (string.match(/[A-Z]/g) || []).length;
-    lowCounter = (string.match(/[a-z]/g) || []).length;
+  upperCounter = (string.match(/[A-Z]/g) || []).length;
+  lowCounter = (string.match(/[a-z]/g) || []).length;
 
-// let newString = string.split("");
-// for (i = 0; i < newString.length; i++) {
-// if (newString[i] == ' ' || newString.match(/\W|_/g) !== null) {
-//   neighterCounter = neighterCounter + 1;
-// }
+  // let newString = string.split("");
+  // for (i = 0; i < newString.length; i++) {
+  // if (newString[i] == ' ' || newString.match(/\W|_/g) !== null) {
+  //   neighterCounter = neighterCounter + 1;
+  // }
 
-// }
+  // }
 
 
-// MY attempt at the last property^
+  // MY attempt at the last property^
 
-neighterCounter = (string.match(neighterRegex) || []).length;
+  neighterCounter = (string.match(neighterRegex) || []).length;
 
-console.log("uppercase: " + upperCounter + ", lowercase: " + lowCounter + ", neighter: " + neighterCounter);
+  console.log(`uppercase: ${upperCounter}, lowercase: ${lowCounter}, neighter: ${neighterCounter}`);
 }
 
 // Don't write below this line...
