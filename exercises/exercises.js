510778014c1710267ebb1c77e2f05793813c9e7c
EC2 Default User <ec2-user@ip-172-31-42-92.ec2.internal>
2019-11-18 16:12:38 +0000

    made changes

--- exercises/exercises.js
+++ exercises/exercises.js
@@ -1,10 +1,10 @@
 // Question 1
 function stripPunctuation(string) {
-    let alphanumeric = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+    const alphanumeric = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
     let empStr ='';
-    for (let i=0; i< string.length; i++){
+    for (let i = 0; i < string.length; i++){
         if (alphanumeric.includes(string[i].toLowerCase())){
-            empStr+=string[i];
+            empStr += string[i];
         }
     }
     return empStr;
@@ -12,16 +12,14 @@ function stripPunctuation(string) {
 
 // Question 2
 function rotateArray(arr) {
-    let newArr=[];
+    let newArr = [];
     let empArr = [];
     if (!Array.isArray(arr)){
         return undefined;
-    }else if (arr.length<=0){
+    }else if (arr.length <= 0){
         return empArr;
     }
-    for (let i=0; i<arr.length; i++){
-        newArr[i]=arr[i];
-    }
+    newArr = arr.slice();
     let firstLetter = newArr.shift();
     newArr.push(firstLetter);
     return newArr;
@@ -29,34 +27,22 @@ function rotateArray(arr) {
 
 // Question 3
 function letterCaseCounts(string) {
-    let obj={};
-    for(let i = 0; i<string.length; i++){
-        if (string[i]===isLower()|| string[i]===isUpper()){
-            
-        }
+    let obj = {
+      lowercase: 0,
+      uppercase: 0,
+      neither: 0
+    };
+    let count = 0;
+    for (let i = 0; i< string.length; i++){
+        if (!string[i].match(/[A-Z]/gi)){
+            obj.neither += 1;
+         }else if (string[i].match(/[A-Z]/g)){
+             obj.uppercase += 1;
+         }else{
+             obj.lowercase += 1;
+         }
     }
-    
-
-}
-
-function isLower(word){
-    let lower= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    for(let i=0; i<word.length;i++){
-        if (lower.includes(word[i])){
-            return true;
-        }
-    }
-    return false;
-}
-
-function isUpper(word){
-    let lower= ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-    for(let i=0; i<word.length;i++){
-        if (lower.includes(word[i])){
-            return true;
-        }
-    }
-    return false;
+    return obj;
 }
 
 // Don't write below this line...
